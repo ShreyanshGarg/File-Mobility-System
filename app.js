e1e9e2906ddd20193e323fc51d7dd0270457408c
Shreyansh Garg <guptashreyansh22@gmail.com>
2022-10-28 12:55:53 +0530

    DB integration, minor change in ejs files,login/signup request integration with db

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config({ path: "config/dev.env" });
-
+const pool = require("./queries.js");
+const { v4: uuidv4 } = require("uuid");
+const e = require("express");
 const app = express();
 
 app.use(express.static("public"));
@@ -11,6 +13,8 @@ app.use(
     extended: true,
   })
 );
+
+pool.connect();
 
 app.get("/officerlogin", function (req, res) {
   res.render("officerlogin");
@@ -30,6 +34,57 @@ app.get("/userdashboard", function (req, res) {
 
 app.get("/officerdashboard", function (req, res) {
   res.render("officer-dashboard");
+});
+
+app.post("/usersignup", function (req, res) {
+  // console.log(req.body);
+  const { firstName, lastname, mobileno, email, password } = req.body;
+  pool.query(
+    "INSERT INTO customer VALUES ($1, $2,$3,$4,$5,$6,$7) RETURNING *",
+    [uuidv4(), firstName, email, password, "{455555}", "1998-10-1", "M"],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.redirect("/userdashboard");
+    }
+  );
+});
+
+app.post("/userlogin", function (req, res) {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  pool.query(
+    "Select * from customer where email=$1",
+    [email],
+    (err, result) => {
+      if (err) console.log(err);
+      else {
+        console.log(result);
+        if (result.rows[0].password == password) res.redirect("/userdashboard");
+        else console.log("Password Incorrect!!");
+      }
+    }
+  );
+});
+
+app.post("/officerlogin", function (req, res) {
+  // console.log(req.body);
+  const { name, username, password } = req.body;
+  pool.query(
+    "Select * from officers where username=$1",
+    [username],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(result);
+        if (result.rows[0].password == password) {
+          res.redirect("/officerdashboard");
+        } else console.log("password not correct!!");
+      }
+    }
+  );
 });
 
 app.listen(process.env.PORT, function () {
