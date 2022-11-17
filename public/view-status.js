7f56f35cfd3ab1d997597b9b0d700adeea614c84
Tanmayjain10 <jain2015tanmay@gmail.com>
2022-11-17 23:12:44 +0530

    User view more rendering

--- public/view-status.js
+++ public/view-status.js
@@ -61,3 +61,79 @@ function openNav() {
     // }
     document.getElementsByClassName("card-wrapper")[0].style.display="block"
   }
+
+function accepted() {
+  document.querySelectorAll(".label").classList.add('green-active-text');
+  document.querySelectorAll(".label").classList.remove('label '); 
+}
+
+const stat = document.getElementById("multi-step-form-container").getAttribute("value");
+const statArray = stat.split(",");
+console.log(statArray);
+
+if(statArray[statArray.length-1] === 'accepted'){
+  const boxes = document.querySelectorAll(".label");
+  const circle = document.querySelectorAll(".form-stepper-circle");
+  document.querySelector(".dis3").style.display = "block";
+  document.querySelector(".dis4").style.display = "block";
+  for(const box of boxes){
+    box.classList.add('green-active-text');
+    box.classList.remove('label');
+  }
+  for(const element of circle){
+    element.style.backgroundColor = "green";
+  }
+} else if(statArray[statArray.length-1] === 'rejected') {
+  if(statArray[statArray.length-2] === 'officer1'){
+    //rejected by officer 1
+    const twos = document.querySelectorAll(".two");
+    document.querySelector(".two-text").innerHTML="Rejected by ofiicer 1";
+    document.querySelector(".circle-two").style.backgroundColor="red";
+    for(const two of twos){
+      two.classList.add('red-active-text');
+      two.classList.remove('label');
+    }
+  } else {
+    //rejected by officer 2
+    const twos = document.querySelectorAll(".two");
+    const threes = document.querySelectorAll(".three");
+    document.querySelector(".three-text").innerHTML="Rejected by ofiicer 2"
+    document.querySelector(".circle-two").style.backgroundColor="green";
+    document.querySelector(".circle-three").style.backgroundColor="red";
+    document.querySelector(".dis3").style.display = "block";
+    for(const two of twos){
+      two.classList.add('green-active-text');
+      two.classList.remove('label');
+    }
+    for(const three of threes){
+      three.classList.add('red-active-text');
+      three.classList.remove('label');
+    }
+  }
+} else if(statArray[statArray.length-1] === 'officer1') {
+  //second color to blue
+  document.querySelector(".circle-two").style.backgroundColor="#4361ee";
+  const twos = document.querySelectorAll(".two");
+  document.querySelector(".two-text").innerHTML="Request pending at Officer 1";
+  for(const two of twos) {
+    two.style.color="#4361ee";
+  }
+} else {
+  // accepted by officer 1
+  const twos = document.querySelectorAll(".two");
+  document.querySelector(".circle-two").style.backgroundColor="green";
+  for(const two of twos){
+    two.classList.add('green-active-text');
+    two.classList.remove('label');
+  }
+
+  //pending at officer 2
+  document.querySelector(".circle-three").style.backgroundColor="#4361ee";
+  const threes = document.querySelectorAll(".three");
+  document.querySelector(".dis3").style.display = "block";
+  document.querySelector(".three-text").innerHTML="Request pending at Officer 1";
+  console.log(threes);
+  for(const three of threes) {
+    three.style.color="#4361ee";
+  }
+}
